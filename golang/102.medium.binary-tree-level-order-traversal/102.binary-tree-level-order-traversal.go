71664e2025920cc1b1a5c665c4b048a2a573e976
wych42 <wych101010@gmail.com>
2021-02-18 18:36:07 +0800

    update

--- golang/102.medium.binary-tree-level-order-traversal/102.binary-tree-level-order-traversal.go
+++ golang/102.medium.binary-tree-level-order-traversal/102.binary-tree-level-order-traversal.go
@@ -1,26 +1,24 @@
-package golang
-
 /*
- * @lc app=leetcode.cn id=102 lang=golang
+ * @lc app=leetcode id=102 lang=golang
  *
- * [102] 二叉树的层序遍历
+ * [102] Binary Tree Level Order Traversal
  *
- * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
  *
  * algorithms
- * Medium (63.57%)
- * Likes:    674
- * Dislikes: 0
- * Total Accepted:    209.2K
- * Total Submissions: 328.9K
+ * Medium (49.03%)
+ * Likes:    4215
+ * Dislikes: 102
+ * Total Accepted:    774.3K
+ * Total Submissions: 1.4M
  * Testcase Example:  '[3,9,20,null,null,15,7]'
  *
- * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
  *
  *
- *
- * 示例：
- * 二叉树：[3,9,20,null,null,15,7],
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
  *
  * ⁠   3
  * ⁠  / \
@@ -29,7 +27,8 @@ package golang
  * ⁠  15   7
  *
  *
- * 返回其层次遍历结果：
+ *
+ * return its level order traversal as:
  *
  * [
  * ⁠ [3],
@@ -49,21 +48,28 @@ package golang
  *     Right *TreeNode
  * }
  */
+// 用队列保存待访问的节点; 用临时变量存储当前层的长度；处理完一层清空临时变量
 func levelOrder(root *TreeNode) [][]int {
 	result := make([][]int, 0)
+
+	// 每一层的数据
+	layer := make([]int, 0)
+
+	// 待访问的队列
 	queue := make([]*TreeNode, 0)
 	queue = append(queue, root)
 
+	// 当前层的大小
 	size := len(queue)
-	layer := make([]int, 0)
-
 	for len(queue) > 0 {
-		if size <= 0 {
+		if size <= 0 { // 一层访问完了
 			result = append(result, layer)
+			// 重置层数据
 			layer = make([]int, 0)
 			size = len(queue)
 		}
 
+		// 出队列
 		node := queue[0]
 		queue = queue[1:]
 		size = size - 1
@@ -71,10 +77,11 @@ func levelOrder(root *TreeNode) [][]int {
 			continue
 		}
 		layer = append(layer, node.Val)
+		// 子节点加入
 		queue = append(queue, node.Left, node.Right)
-
 	}
 	return result
 }
 
 // @lc code=end
+
