60b59fb914beff185cf20fb32aec03ccc740b17a
wych42 <wych101010@gmail.com>
2021-03-03 22:58:50 +0800

    update

--- golang/117.medium.populating-next-right-pointers-in-each-node-ii/117.populating-next-right-pointers-in-each-node-ii.go
+++ golang/117.medium.populating-next-right-pointers-in-each-node-ii/117.populating-next-right-pointers-in-each-node-ii.go
@@ -1,21 +1,19 @@
 /*
- * @lc app=leetcode id=116 lang=golang
+ * @lc app=leetcode id=117 lang=golang
  *
- * [116] Populating Next Right Pointers in Each Node
+ * [117] Populating Next Right Pointers in Each Node II
  *
- * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
  *
  * algorithms
- * Medium (48.98%)
- * Likes:    3120
- * Dislikes: 161
- * Total Accepted:    463.9K
- * Total Submissions: 944.3K
- * Testcase Example:  '[1,2,3,4,5,6,7]'
+ * Medium (42.01%)
+ * Likes:    2287
+ * Dislikes: 196
+ * Total Accepted:    320.2K
+ * Total Submissions: 760.3K
+ * Testcase Example:  '[1,2,3,4,5,null,7]'
  *
- * You are given a perfect binary tree where all leaves are on the same level,
- * and every parent has two children. The binary tree has the following
- * definition:
+ * Given a binary tree
  *
  *
  * struct Node {
@@ -47,20 +45,21 @@
  *
  *
  *
- * Input: root = [1,2,3,4,5,6,7]
- * Output: [1,#,2,3,#,4,5,6,7,#]
- * Explanation: Given the above perfect binary tree (Figure A), your function
- * should populate each next pointer to point to its next right node, just like
- * in Figure B. The serialized output is in level order as connected by the
- * next pointers, with '#' signifying the end of each level.
+ * Input: root = [1,2,3,4,5,null,7]
+ * Output: [1,#,2,3,#,4,5,7,#]
+ * Explanation: Given the above binary tree (Figure A), your function should
+ * populate each next pointer to point to its next right node, just like in
+ * Figure B. The serialized output is in level order as connected by the next
+ * pointers, with '#' signifying the end of each level.
  *
  *
  *
  * Constraints:
  *
  *
- * The number of nodes in the given tree is less than 4096.
- * -1000 <= node.val <= 1000
+ * The number of nodes in the given tree is less than 6000.
+ * -100 <= node.val <= 100
+ *
  *
  */
 
@@ -75,7 +74,6 @@
  * }
  */
 
-// 就是一个前序遍历
 func connect(root *Node) *Node {
 	if root == nil {
 		return root
@@ -108,8 +106,14 @@ func connect(root *Node) *Node {
 		if node == nil {
 			continue
 		}
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
 
-		queue = append(queue, node.Left, node.Right)
+		// queue = append(queue, node.Left, node.Right)
 		layer = append(layer, node)
 	}
 	return root
