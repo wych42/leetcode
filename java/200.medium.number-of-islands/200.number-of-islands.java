dbce561ced302f7cf6672be0fa1c3a59c8e36b02
wych42 <wych101010@gmail.com>
2021-04-14 23:29:21 +0800

    java: number of islands

---
+++ java/200.medium.number-of-islands/200.number-of-islands.java
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=200 lang=java
+ *
+ * [200] Number of Islands
+ *
+ * https://leetcode.com/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (49.01%)
+ * Likes:    8164
+ * Dislikes: 241
+ * Total Accepted:    1M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+ *
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and
+ * '0's (water), return the number of islands.
+ * 
+ * An island is surrounded by water and is formed by connecting adjacent lands
+ * horizontally or vertically. You may assume all four edges of the grid are
+ * all surrounded by water.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: grid = [
+ * ⁠ ["1","1","1","1","0"],
+ * ⁠ ["1","1","0","1","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: grid = [
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","1","0","0"],
+ * ⁠ ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] is '0' or '1'.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int ans = 0;
+        for (int row =0;row<rows;row++) {
+            for (int col=0;col<cols;col++) {
+                if (grid[row][col] == '1') {
+                    dfs(grid, row, col);
+                    ans++;
+                }
+
+            }
+        }
+        return ans;
+    }
+    public void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) {
+            return;
+        }
+        if (grid[row][col]!='1') {
+            return;
+        }
+        grid[row][col] = '2';
+        dfs(grid, row-1, col);
+        dfs(grid, row+1, col);
+        dfs(grid, row, col-1);
+        dfs(grid, row, col+1);
+    }
+}
+// @lc code=end
+
