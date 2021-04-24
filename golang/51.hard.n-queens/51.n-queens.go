10be3a470cdf5ff4ef590b577406354a5b72428b
wych42 <wych101010@gmail.com>
2021-04-24 22:04:31 +0800

    golang n-queens(update)

--- golang/51.hard.n-queens/51.n-queens.go
+++ golang/51.hard.n-queens/51.n-queens.go
@@ -52,8 +52,6 @@
 import "strings"
 import "math"
 
-var answerSets [][]int
-
 func solveNQueens(n int) [][]string {
 	/*
 		backtrack 问题：
@@ -62,36 +60,21 @@ func solveNQueens(n int) [][]string {
 		3. 尝试摆下一行；
 	*/
 	// 每个Queen放一行，每个 []int 记录每一行上 Queen 所在列
-	answerSets = make([][]int, 0)
-	backtrack(n, 0, []int{})
-	result := make([][]string, 0, len(answerSets))
-	for _, answer := range answerSets {
-		matrix := make([]string, n)
-		for row, col := range answer {
-			matrix[row] = strings.Repeat(".", col) + "Q" + strings.Repeat(".", n-col-1)
-		}
-		result = append(result, matrix)
-	}
-	return result
+	answers := make([][]string, 0)
+	backtrack(n, 0, []int{}, &answers)
+	return answers
 }
 
-func backtrack(n int, row int, path []int) {
+func backtrack(n int, row int, path []int, answers *[][]string) {
 	/*
 		n: 总行数
 		row: 当前处理哪一行
 		path: 这一次递归里，每一行已经摆放的 Queen 所在列
 		ans: 结果
 	*/
-	// Goal
+	// capture(goal)
 	if row == n { // 所有行都摆上了
-		//fmt.Println("found answer", path)
-		answer := make([]int, len(path))
-		// golang 语法的坑，这里不能直接加到结果里
-		for i := 0; i < len(path); i++ {
-			answer[i] = path[i]
-		}
-		answerSets = append(answerSets, answer)
-		//fmt.Println(answerSets)
+		*answers = append(*answers, pathToAnswer(path))
 		return
 	}
 	// 在当前行上，尝试在每一列上摆 Queen
@@ -101,7 +84,7 @@ func backtrack(n int, row int, path []int) {
 		// constrain
 		if isValid(path) {
 			// explore
-			backtrack(n, row+1, path)
+			backtrack(n, row+1, path, answers)
 		}
 		// unchoose
 		path = path[:len(path)-1]
@@ -122,6 +105,15 @@ func isValid(path []int) bool {
 		}
 	}
 	return true
+}
+
+func pathToAnswer(path []int) []string {
+	n := len(path)
+	res := make([]string, len(path))
+	for row, col := range path {
+		res[row] = strings.Repeat(".", col) + "Q" + strings.Repeat(".", n-col-1)
+	}
+	return res
 }
 
 // @lc code=end
