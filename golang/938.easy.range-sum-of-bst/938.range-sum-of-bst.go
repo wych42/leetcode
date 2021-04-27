7349e0d2648d76de11abe6b5792d9f010a3c58cc
wych42 <wych101010@gmail.com>
2021-04-27 23:14:00 +0800

    range sum of bst

---
+++ golang/938.easy.range-sum-of-bst/938.range-sum-of-bst.go
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=938 lang=golang
+ *
+ * [938] Range Sum of BST
+ *
+ * https://leetcode.com/problems/range-sum-of-bst/description/
+ *
+ * algorithms
+ * Easy (83.27%)
+ * Likes:    2287
+ * Dislikes: 295
+ * Total Accepted:    369.1K
+ * Total Submissions: 443.1K
+ * Testcase Example:  '[10,5,15,3,7,null,18]\n7\n15'
+ *
+ * Given the root node of a binary search tree, return the sum of values of all
+ * nodes with a value in the range [low, high].
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+ * Output: 32
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+ * Output: 23
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [1, 2 * 10^4].
+ * 1 <= Node.val <= 10^5
+ * 1 <= low <= high <= 10^5
+ * All Node.val are unique.
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	if root == nil {
+		return 0
+	}
+	// 中序遍历
+	sum := 0
+	sum += rangeSumBST(root.Left, low, high)
+	if root.Val >= low && root.Val <= high {
+		sum += root.Val
+	}
+	sum += rangeSumBST(root.Right, low, high)
+	return sum
+}
+
+// @lc code=end
+
