46c61962f605b9513abcb20bdf686414a806d8a3
wych42 <wych101010@gmail.com>
2023-07-11 22:25:25 +0800

    grind 75 #3 merge two sorted list

---
+++ java/21.easy.merge-two-sorted-lists/21.merge-two-sorted-lists.java
@@ -0,0 +1,127 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.cn/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (66.18%)
+ * Likes:    3159
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * 
+ * Merge the two lists in a one sorted list. The list should be made by
+ * splicing together the nodes of the first two lists.
+ * 
+ * Return the head of the merged linked list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode merged;
+        if (list1.val <= list2.val) {
+            merged = list1;
+            list1 = list1.next;
+        } else {
+            merged = list2;
+            list2 = list2.next;
+        }
+        // point to current node
+        ListNode cur = merged;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if (list1 != null) {
+            cur.next = list1;
+        } 
+        if (list2 != null) {
+            cur.next = list2;
+        }
+        return merged;
+    }
+
+    // public class ListNode {
+    //     int val;
+
+    //     ListNode next;
+
+    //     ListNode() {}
+
+    //     ListNode(int val) { 
+    //         this.val = val;
+    //     }
+
+    //     ListNode(int val, ListNode next) {
+    //         this.val = val;
+    //         this.next = next;
+    //     }
+
+    // }
+}
+// @lc code=end
+
